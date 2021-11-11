3586d503a53c9268db78b739ac7b651108e202a8
Quaris <quaris314@gmail.com>
2021-11-11 16:40:51 -0600

    Updated Semester.java

--- spikes/java/src/semesters/Semester.java
+++ spikes/java/src/semesters/Semester.java
@@ -9,33 +9,36 @@ import java.util.LinkedList;
 import course.Course;
 
 /**
- * Complete 29 Oct
+ * Complete 11 Nov
  * @author Quaris
  *
  */
 public class Semester implements Comparable<Semester> {
 
-	private SemesterList container;
-	
-	private int n;
+	private int year;
 	private Season season;
 	private StudentYear studentYear;
-	private LinkedList<Course> courses;
-	private int totalCredits;
+	private CourseList courses;
 	
-	public Semester(SemesterList container, int n, Season season, StudentYear studentYear) {
-		this.container = container;
-		this.n = n;
-		this.setSeason(season);
-		this.setStudentYear(studentYear);
-		this.courses = new LinkedList<Course>();
-		this.totalCredits = 0;
+	public Semester(int year, Season season, StudentYear studentYear) {
+		this.year = year;
+		this.season = season;
+		this.studentYear = studentYear;
+		this.courses = new CourseList();
 	}
 
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
 	public Season getSeason() {
 		return season;
 	}
-
+	
 	public void setSeason(Season season) {
 		this.season = season;
 	}
@@ -43,83 +46,36 @@ public class Semester implements Comparable<Semester> {
 	public StudentYear getStudentYear() {
 		return studentYear;
 	}
-
+	
 	public void setStudentYear(StudentYear studentYear) {
 		this.studentYear = studentYear;
 	}
 	
-	/**
-	 * Does not add course if already present in this semester
-	 * @param course
-	 */
-	public void addCourse(Course course) {
-		if (!courses.contains(course)) {
-			if (courses.isEmpty() && container != null
-					&& this != container.getUnplanned() 
-					&& this != container.getSatisfied()) {
-				container.getSemesterAfter(this);
-			}
-			courses.add(course);
-			totalCredits += course.getCredits();
-			if (container != null) {
-				container.refreshYears();
-				
-			}
-		}
-	}
-	
-	public void removeCourse(Course course) {
-		courses.remove(course);
-		totalCredits -= course.getCredits();
-		if (container != null) {
-			container.refreshYears();
-		}
-	}
-	
-	public boolean hasCourse(Course course) {
-		return courses.contains(course);
-	}
-	
-	public Course[] getCourses() {
-		// deep copy
-		Course[] courseArr = courses.toArray(new Course[courses.size()]);
-		Arrays.sort(courseArr);
-		return courseArr;
-	}
-	
-	public int getTotalCredits() {
-		return totalCredits;
-	}
-	
-	public int getN() {
-		return n;
+	public CourseList getCourses() {
+		return courses;
 	}
 	
 	public String getName() {
-		if (this == container.getUnplanned()) {
-			return "Unplanned";
-		} else if (this == container.getSatisfied()) {
-			return "Satisfied";
-		} else if (season == null) {
-			return "Unknown " + String.valueOf(n);
-		} else {
-			return season.toString() + " " + String.valueOf(n) + " - " + studentYear.toString();
-		}
+		StringBuilder s = new StringBuilder();
+		s.append(year > 0 ? year : "-").append(" ");
+		s.append(season != null ? season : "-").append(", ");
+		s.append(studentYear != null ? studentYear : "-");
+		return s.toString();
 	}
 	
 	@Override
 	public int compareTo(Semester semester) {
-		return this.n - semester.n;
+		return this.year * 4 + this.season.ordinal() - (semester.year * 4 + semester.season.ordinal());
 	}
 	
 	@Override
 	public String toString() {
 		StringBuilder s = new StringBuilder();
-		s.append(this.getName()).append("\n");
+		s.append(this.getName()).append('\n');
 		for (Course course : courses) {
 			s.append("  ").append(course.toString()).append('\n');
 		}
-		s.append("  Credits: ").append(this.getTotalCredits()).append('\n');
+		s.append("  Credits: ").append(this.courses.getTotalCredits()).append('\n');
 		return s.toString();
 	}
 
